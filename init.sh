1c5d85f69eacc573968303b4092b4e5f8d9d837e
Javier Parada <javierparadadev@gmail.com>
2024-03-19 21:30:26 +0900

    Load .bk repo for backup

--- init.sh
+++ init.sh
@@ -2,15 +2,14 @@
 
 # Initialization of variables
 USER=""
-DOMAIN="github.com" # Default value
-REPOS_FILE="repos.txt" # The file from which to load the repository names
+REPO_NAME=".bk"
+FILE_NAME="repos.txt"
 OUTPUT_DIR="" # Output directory for the repositories
 
 # Command line arguments processing
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --user) USER="$2"; shift ;;
-        --domain) DOMAIN="$2"; shift ;;
         -o|--output) OUTPUT_DIR="$2"; shift ;;
         *) echo "Unknown option: $1"; exit 1 ;;
     esac
@@ -22,36 +21,38 @@ if [[ -z "$USER" ]]; then
     exit 1
 fi
 
-if [ ! -f "$REPOS_FILE" ]; then
-    echo "Repository list file $REPOS_FILE does not exist."
+# Construct the URL to download the repos.txt file
+REPOS_URL="https://raw.githubusercontent.com/${USER}/${REPO_NAME}/main/${FILE_NAME}"
+
+# Use curl to download the list of repositories
+echo "Downloading repository list from $REPOS_URL"
+REPOS=$(curl -sS $REPOS_URL)
+if [ $? -ne 0 ]; then
+    echo "Failed to download the repository list."
     exit 1
 fi
 
-# Use the directory of the repos file as the default output directory if none specified
+# Check if an output directory was specified, else use current directory
 if [[ -z "$OUTPUT_DIR" ]]; then
-    OUTPUT_DIR=$(dirname "$REPOS_FILE")
+    OUTPUT_DIR=$(pwd)
 fi
 
 # Ensure the output directory exists
 mkdir -p "$OUTPUT_DIR"
 
-# Read repository names from the file
-REPOS=()
-while IFS= read -r line; do
-    REPOS+=("$line")
-done < "$REPOS_FILE"
-
-for repo in "${REPOS[@]}"; do
-  dir="${OUTPUT_DIR}/${repo}"
-  repo_url="https://$DOMAIN/$USER/$repo.git"
-  echo "Processing $repo_url in $dir"
-  if [ -d "$dir" ]; then
-    echo "Updating $dir..."
-    cd "$dir"
-    git pull
-    cd -  # Returns to the previous directory
-  else
-    echo "Cloning into $dir..."
-    git clone "$repo_url" "$dir"
-  fi
+# Process each repository name
+echo "$REPOS" | while read repo; do
+    if [[ -z "$repo" ]]; then continue; fi # Skip empty lines
+    dir="${OUTPUT_DIR}/${repo}"
+    repo_url="https://github.com/${USER}/${repo}.git"
+    echo "Processing $repo_url in $dir"
+    if [ -d "$dir" ]; then
+        echo "Updating $dir..."
+        cd "$dir"
+        git pull
+        cd -  # Returns to the previous directory
+    else
+        echo "Cloning into $dir..."
+        git clone "$repo_url" "$dir"
+    fi
 done
